86562698b98299a4305e1f03090660713d7bb27e
Nathan Rosenboom <nathan.rosenboom@gmail.com>
2020-07-01 22:33:48 +0800

    Created search result unordered list component

---
+++ src/components/SearchResultUL/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+function SearchResults() {
+
+  return (
+    <ul className="list-group search-results">
+
+    </ul>
+  );
+}
+
+export default SearchResults;
