1605264bd349a8b834edc90617c9b58cec799c0f
Nathan Rosenboom <nathan.rosenboom@gmail.com>
2020-07-04 01:52:46 +0800

    Added filtering functionality to input field

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -8,23 +8,11 @@ function SearchForm(props) {
       <div className="form-group">
         <label htmlFor="filter">Search:</label>
         <input
-          value={props.search}
-          onChange={props.handleInputChange}
-          name="employee"
-          list="employees"
           type="text"
           className="form-control"
+          onChange={props.Userfilter}
           placeholder="Enter employee details to filter"
-          id="employee"
         />
-        <datalist id="employees">
-          {props.employees.map(employee => (
-            <option value={employee} key={employee} />
-          ))}
-        </datalist>
-        <button type="submit" onClick={props.handleFormSubmit} className="btn btn-success">
-          Search
-        </button>
       </div>
     </form>
   );
